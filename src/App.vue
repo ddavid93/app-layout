ccf8375d74735180882427be1c314b8dfbbc66a2
DavidDiaz <david.diaz@yanovis.com>
2022-08-24 13:36:50 +0200

    Menu working ok

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
             <loading :active="!$auth.ready()" :is-full-page="true"></loading>
         </div>
 
-        <CoreDrawer :dynamic="menu"/>
+        <CoreDrawer :dynamic="conf"/>
 
         <CoreFooter/>
 
@@ -22,30 +22,11 @@
 <script setup>
 import {components} from '@Yanovis/app-components'
 import Loading from 'vue-loading-overlay'
-import i18n from '../src/plugins/i18n'
+import {onMounted, ref} from 'vue'
+import {menu} from '@Yanovis/app-utils'
 
-const menu = [
-    {
-        show: true,
-        // show: this.$auth.check('act.service.index'),
-        route: 'activities.services',
-        icon: '$vuetify.icons.ynv-calendar',
-        title: i18n.t('activities.services.servicesDescription')
-    },
-    {
-        show: true,
-        // show: this.$auth.check('book.order.create'),
-        route: 'activities.bookings',
-        icon: '$vuetify.icons.ynv-cart',
-        title: i18n.t('activities.services.booking.bookingDescription')
-    },
-    {
-        show: true,
-        // show: this.$auth.check('book.order.index'),
-        route: 'activities.orders',
-        icon: '$vuetify.icons.ynv-person-circle',
-        title: i18n.t('activities.order.orderDescription')
-    }
-]
 const {CoreToolbar, CoreFooter, CoreDrawer} = components
+const conf = ref(menu.menu)
+
+onMounted(() => menu.menu$.subscribe(data => conf.value = data))
 </script>
