4a12068389b2be0124e8b8a6c861ae6b02ed7f7e
ddavid930826@gmail.com <ddavid930826@gmail.com>
2022-09-07 13:21:11 +0200

    no routes file

--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-console.log('%c App-Layout Loaded', 'background: #222; color: #bada55')
-
+import VueRouter from "vue-router";
 import Vue from 'vue';
 import singleSpaVue from 'single-spa-vue';
-import router from '../src/router'
+
 import App from './App.vue';
 import {authMixin, i18n, vuetify} from "@Yanovis/app-components";
+
+console.log('%c App-Layout Loaded', 'background: #222; color: #bada55')
 
 Vue.config.productionTip = false;
 
 const app = singleSpaVue({
-    Vue,
-    appOptions: {
-        i18n,
-        vuetify,
-        router,
-        mixins: [authMixin],
-        render: h => h(App),
-    },
-});
+        Vue,
+        appOptions: {
+            i18n,
+            vuetify,
+            router: new VueRouter({mode: "history"}),
+            mixins: [authMixin],
+            render: h => h(App),
+        },
+    })
+;
 
 
 export const {bootstrap, mount, unmount, update} = app
